4fc095538d2badc9b820ee098375c3a7c6ab5777
AlAmeenBasheer <134540107+AlAmeenBasheer@users.noreply.github.com>
2023-05-24 23:07:59 +0530

    Add files via upload

---
+++ CommandLineExample.java
@@ -0,0 +1,8 @@
+public class CommandLineExample {
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+		System.out.println("Name:"+args[0]);
+		System.out.println("Roll number:"+args[1]);
+		
+	}
+}
